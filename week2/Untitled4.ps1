194e5f1421ea81a0ac7467fe4e1fe41f2145b947
nydia <nydia.lane@mymail.champlain.edu>
2025-02-05 14:58:26 -0500

    hi

---
+++ week2/Untitled4.ps1
@@ -0,0 +1,6 @@
+﻿$chromeProcess = Get-Process -Name chrome -ErrorAction SilentlyContinue
+if ($chromeProcess) {
+    Stop-Process -Name chrome -Force 
+        } else { 
+        Start-Process "chrome.exe" -ArgumentList "https://www.champlain.edu"
+        }
---
+++ week4/Untitled1.ps1
@@ -0,0 +1,18 @@
+﻿# logs that only contain 404, saves to $notfound
+$notfounds = Get-Item C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+# defines regex from IP addresses
+$regex = [regex] "\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+# get $notfound records that match the regex
+$ipsUnorganized = $regex.Matches($notfounds)
+$ipsUnorganized.Value
+# get ips as pscustomobject
+$ips = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+$ips +=[pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+}
+# $ips | where-Object { $_.IP -ilike "10.*" }
+
+# counts the IPs
+$counts = $ips |  Where-Object { $_.IP -ilike "10.*" } | Group-Object -Property "IP"
+$counts | Select-Object Count, Name
